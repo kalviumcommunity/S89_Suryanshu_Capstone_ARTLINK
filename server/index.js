3f43f4f623ad97cc7fa10535b5820a09cc6e8c9f
suryanshu2006 <singhsuryanshukumar5@gmail.com>
2025-04-24 10:53:43 +0530

    post request

--- server/index.js
+++ server/index.js
@@ -8,6 +8,18 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 8080;
 
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("MongoDB connected successfully");
+}).catch((error) => {
+    console.log("MongoDB connection failed", error);
+})
 
 
 app.get("/",(request, response) => {
@@ -18,7 +30,7 @@ app.get("/",(request, response) => {
     }    
 })
 
-app.listen(8080,async() => {
+app.listen(5000,async() => {
     try {
        
         console.log("server connected");
